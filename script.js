f6247e43212d797824b8face5910a79a0ce0b4ae
randydiaz01 <randy.diaz01@ufl.edu>
2016-09-14 19:34:40 -0400

    Add files via upload
    My first Website!! 
    -Resume Website-

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    setInterval ('cursorAnimation()', 800);
+});
+
+
+function cursorAnimation() {
+    $('#cursor').animate({
+        opacity: 0
+    }, 'fast', 'swing').animate({
+        opacity: 1
+    }, 'fast', 'swing');
+}
